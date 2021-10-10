497597e234a4d7ccafbde6274093cb5ef20240c3
Andreas Gruenwald <andreas@grynwald.net>
2021-10-10 18:20:11 +0200

    Add Cake annotation attributes to GitHubReleaseAliases

--- src/Cake.GitHubReleases/GitHubReleaseAliases.cs
+++ src/Cake.GitHubReleases/GitHubReleaseAliases.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Cake.Core;
+using Cake.Core.Annotations;
 using Cake.GitHubReleases.Internal;
 
 namespace Cake.GitHubReleases
 {
+    [CakeAliasCategory("GitHubRelease")]
     public static class GitHubReleaseAliases
     {
         /// <summary>
-        /// Creates a new GitHub Release with the specified settings
+        /// Creates a new GitHub Release with the specified settings.
         /// </summary>
+        [CakeMethodAlias]
         public static async Task<GitHubRelease> GitHubReleaseCreateAsync(this ICakeContext context, GitHubReleaseCreateSettings settings)
         {
             if (settings is null)
