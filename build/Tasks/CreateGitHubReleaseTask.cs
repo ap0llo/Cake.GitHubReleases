485a01c3cc5362f38f75361e8dae77715c5477a5
Andreas Gruenwald <andreas@grynwald.net>
2021-10-11 13:42:08 +0200

    ci: Create a non-draft GitHub release when building release branches

--- build/Tasks/CreateGitHubReleaseTask.cs
+++ build/Tasks/CreateGitHubReleaseTask.cs
@@ -11,48 +11,69 @@ namespace Build.Tasks
     [IsDependentOn(typeof(GenerateChangeLogTask))]
     public class CreateGitHubReleaseTask : AsyncFrostingTask<BuildContext>
     {
-
         public override bool ShouldRun(BuildContext context)
         {
-            return context.IsRunningInCI && context.IsMasterBranch;
+            return context.IsRunningInCI && (context.IsMasterBranch || context.IsReleaseBranch);
         }
 
         public override async Task RunAsync(BuildContext context)
         {
-            //
-            // For builds on master, create a *draft* release
-            //
-            context.Log.Information("Creating GitHub draft release");
+            context.Log.Information("Creating GitHub Releases");
+
             var versionInfo = context.GitVersioningGetVersion(context.RootDirectory.FullPath);
-            var releaseName = versionInfo.NuGetPackageVersion;
 
             var changeLog = context.FileSystem.GetFile(context.ChangeLogOutputPath).ReadAllText();
 
-            var releaseSettings = new GitHubReleaseCreateSettings(context.GitHubProject.RepositoryOwner, context.GitHubProject.RepositoryName, "vNext")
-            {
-                Draft = true,
-                Prerelease = true,
-                Name = releaseName,
-                Body = changeLog,
-                Overwrite = true,
-                TargetCommitish = context.SourceVersion,
-            };
-
-            if (context.TryGetGitHubAccessToken() is string accessToken)
-            {
-                releaseSettings.AccessToken = accessToken;
-            }
-            else
+            if (context.TryGetGitHubAccessToken() is not string accessToken)
             {
                 throw new Exception("No GitHub access token specified. Cannot create a GitHub Release");
             }
 
-            context.Log.Information($"Creating release '{releaseSettings.Name}' for commit '{releaseSettings.TargetCommitish}'");
+            //
+            // For builds on master, create a *draft* release
+            //
+            if (context.IsMasterBranch)
+            {
+                await CreateDraftRelease(context, accessToken, versionInfo.NuGetPackageVersion, changeLog);
+            }
+
+            //
+            // For builds on release branches, create a non-draft release
+            //
+            if (context.IsReleaseBranch)
+            {
+                await CreateRelease(context, accessToken, versionInfo.NuGetPackageVersion, changeLog);
+            }
+        }
+
+
+        private async Task CreateDraftRelease(BuildContext context, string accessToken, string version, string changeLog)
+        {
+            var releaseSettings = new GitHubReleaseCreateSettings(context.GitHubProject.RepositoryOwner, context.GitHubProject.RepositoryName, "vNext")
+            {
+                Draft = true,
+                Prerelease = true,
+                Name = $"v{version}",
+                Body = changeLog,
+                Overwrite = true,
+                TargetCommitish = context.SourceVersion,
+                AccessToken = accessToken
+            };
+
             await context.GitHubReleaseCreateAsync(releaseSettings);
+        }
 
+        private async Task CreateRelease(BuildContext context, string accessToken, string version, string changeLog)
+        {
+            var releaseSettings = new GitHubReleaseCreateSettings(context.GitHubProject.RepositoryOwner, context.GitHubProject.RepositoryName, $"v{version}")
+            {
+                Name = $"v{version}",
+                Body = changeLog,
+                TargetCommitish = context.SourceVersion,
+                AccessToken = accessToken
+            };
 
-
-            //TODO: For release builds, create non-draft release
+            await context.GitHubReleaseCreateAsync(releaseSettings);
         }
     }
 }
